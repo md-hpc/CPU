5fa549dc53fbff784154aff3f10df88c2a09d235
skakigi <skakigi@bu.edu>
2025-02-06 21:36:13 -0500

    cleanup code

--- MD.c
+++ MD.c
@@ -9,137 +9,46 @@
 #include <omp.h>
 #include <time.h>
 #include <string.h>
-#define NUM_PARTICLES_UNIVERSE 2
-#define NUM_TIMESTEP 2000000
+#define NUM_PARTICLES_UNIVERSE 10000
+#define NUM_TIMESTEP 1
 #define NUM_THREADS 16
-#define BSIZE 2
+#define BSIZE 1
 #define BLOCKED 1
-void plot_particles(Particle *particleList, FILE *fp)
-{
 
-    for (int i = 0; i < NUM_PARTICLES_UNIVERSE; i++)
-    {
-        fprintf(fp, "%lf %lf %lf\n", particleList[i].x, particleList[i].y, particleList[i].z);
-    }
-    fclose(fp);
-}
+void velocityUpdateBlocked(Particle *particleList);
+void velocityUpdate(Particle *particleList);
+void printData(Particle *particleList);
+void plot_particles(Particle *particleList,FILE *fp);
+
 int main()
 {
     double start, end;
     double cpu_time;
 
-    start = omp_get_wtime();
+    
 
     Particle *particleList = malloc(sizeof(Particle) * NUM_PARTICLES_UNIVERSE);
-    //Acceleration *accelerationCache = malloc(sizeof(Acceleration) * NUM_PARTICLES_UNIVERSE);
     init_ParticleList(particleList);
-    Particle vec, current;
-    float r, f;
+    Particle current;
 
-    FILE *fpBefore = fopen("Before.dat", "w");
-    plot_particles(particleList, fpBefore);
-    printf("Printing Particle List!!\n --------------------\n");
-    for (int i = 0; i < NUM_PARTICLES_UNIVERSE; i++)
-    {
-        printf("Particle:%d\n", particleList[i].particleId);
-        printf("X: %f\n", particleList[i].x);
-        printf("Y: %f\n", particleList[i].y);
-        printf("Z: %f\n", particleList[i].z);
-        printf("\n");
-    }
-    printf("Printing Particle Velocity!!\n --------------------\n");
-    for (int i = 0; i < NUM_PARTICLES_UNIVERSE; i++)
-    {
-        printf("Particle:%d\n", particleList[i].particleId);
-        printf("X: %f\n", particleList[i].vX);
-        printf("Y: %f\n", particleList[i].vY);
-        printf("Z: %f\n", particleList[i].vZ);
-        printf("\n");
-    }
+    //FILE *fpBefore = fopen("Before.dat", "w");
+    //plot_particles(particleList, fpBefore);
 
+    
+    start = omp_get_wtime();
     for (int i = 0; i < NUM_TIMESTEP; i++)
     {
 
-        //init_AccelerationCache(accelerationCache);
-
         if (BLOCKED)
         {
-            #pragma omp parallel for num_threads(NUM_THREADS)
-            for (int ii = 0; ii < NUM_PARTICLES_UNIVERSE; ii += BSIZE)
-            {
-                for (int jj = 0; jj < NUM_PARTICLES_UNIVERSE; jj += BSIZE)
-                {
-                    for (int ref = ii; ref < ii + BSIZE; ref++)
-                    {
-                        for (int neighbor = jj; neighbor < jj + BSIZE; neighbor++)
-                        {
-                            if (ref == neighbor)
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                modr(&vec, &particleList[ref], &particleList[neighbor]);
-                                r = norm(&vec);
-
-                                if (r > CUTOFF)
-                                {
-                                    continue;
-                                }
-
-                                if (vec.x < 0)
-                                {
-                                    continue;
-                                }
-
-                                f = lj(r);
-                                scalar_mul(&vec, DT * f / r);
-                                vec_add(&particleList[ref], &vec);
-                                scalar_mul(&vec, -1.);
-                                vec_add(&particleList[neighbor], &vec);
-                            }
-                        }
-                    }
-                }
-            }
+            velocityUpdateBlocked(particleList);
+           
         }
         else
         {
-            #pragma omp parallel for num_threads(NUM_THREADS)
-            for (int ref = 0; ref < NUM_PARTICLES_UNIVERSE; ref++)
-            {
-                for (int neighbor = 0; neighbor < NUM_PARTICLES_UNIVERSE; neighbor++)
-                {
-                    if (ref == neighbor)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-
-                        modr(&vec, &particleList[ref], &particleList[neighbor]);
-                        r = norm(&vec);
-
-                        if (r > CUTOFF)
-                        {
-                            continue;
-                        }
-
-                        if (vec.x < 0)
-                        {
-                            continue;
-                        }
-
-                        f = lj(r);
-                        scalar_mul(&vec, DT * f / r);
-                        vec_add(&particleList[ref], &vec);
-                        scalar_mul(&vec, -1.);
-                        vec_add(&particleList[neighbor], &vec);
-
-                    }
-                }
-            }
+            velocityUpdate(particleList);
         }
+
         for (int ref = 0; ref < NUM_PARTICLES_UNIVERSE; ref++)
         {
             current = particleList[ref];
@@ -157,6 +66,115 @@ int main()
             current.z = current.z > L ? current.z - L : current.z;
         }
     }
+    end = omp_get_wtime();
+    cpu_time = (double)(end - start);
+    //FILE *fpAfter = fopen("After.dat", "w");
+    //plot_particles(particleList, fpAfter);
+
+
+    printf("Time Taken: %f seconds\n", cpu_time);
+    return 0;
+}
+void init_ParticleList(Particle *particleList)
+{
+    srand(SEED);
+    int min = -10;
+    int max = 10;
+
+    for (int i = 0; i < NUM_PARTICLES_UNIVERSE; i++)
+    {
+        particleList[i].x = ((float)rand() / (float)(RAND_MAX)) * UNIVERSE_SIZE;
+        particleList[i].y = ((float)rand() / (float)(RAND_MAX)) * UNIVERSE_SIZE;
+        particleList[i].z = ((float)rand() / (float)(RAND_MAX)) * UNIVERSE_SIZE;
+
+        particleList[i].vX = ((float)(min + rand() % (max - min + 1)) / (float)max) * UNIVERSE_SIZE / 2;
+        particleList[i].vY = ((float)(min + rand() % (max - min + 1)) / (float)max) * UNIVERSE_SIZE / 2;
+        particleList[i].vZ = ((float)(min + rand() % (max - min + 1)) / (float)max) * UNIVERSE_SIZE / 2;
+
+        particleList[i].particleId = i;
+    }
+}
+void velocityUpdateBlocked(Particle *particleList)
+{
+    Particle vec;
+    float r, f;
+    //#pragma omp parallel for num_threads(NUM_THREADS)
+    for (int ii = 0; ii < NUM_PARTICLES_UNIVERSE; ii += BSIZE)
+    {
+        for (int jj = 0; jj < NUM_PARTICLES_UNIVERSE; jj += BSIZE)
+        {
+            for (int ref = ii; ref < ii + BSIZE; ref++)
+            {
+                for (int neighbor = jj; neighbor < jj + BSIZE; neighbor++)
+                {
+                    if (ref == neighbor)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        modr(&vec, &particleList[ref], &particleList[neighbor]);
+                        r = norm(&vec);
+                        if (r > CUTOFF)
+                        {
+                            continue;
+                        }
+                        if (vec.x < 0)
+                        {
+                            continue;
+                        }
+                        f = lj(r);
+                        scalar_mul(&vec, DT * f / r);
+                        vec_add(&particleList[ref], &vec);
+                        scalar_mul(&vec, -1.);
+                        vec_add(&particleList[neighbor], &vec);
+                    }
+                }
+            }
+        }
+    }
+}
+void velocityUpdate(Particle *particleList)
+{
+    Particle vec;
+    float r, f;
+    #pragma omp parallel for num_threads(NUM_THREADS)
+    for (int ref = 0; ref < NUM_PARTICLES_UNIVERSE; ref++)
+    {
+        for (int neighbor = 0; neighbor < NUM_PARTICLES_UNIVERSE; neighbor++)
+        {
+            
+            if (ref == neighbor)
+            {
+                continue;
+            }
+            else
+            {
+
+                modr(&vec, &particleList[ref], &particleList[neighbor]);
+                r = norm(&vec);
+
+                if (r > CUTOFF)
+                {
+                    continue;
+                }
+
+                if (vec.x < 0)
+                {
+                    continue;
+                }
+
+                f = lj(r);
+                scalar_mul(&vec, DT * f / r);
+                vec_add(&particleList[ref], &vec);
+                scalar_mul(&vec, -1.);
+                vec_add(&particleList[neighbor], &vec);
+                
+            }
+        }
+    }
+}
+void printData(Particle *particleList){
     printf("Printing Particle List!!\n --------------------\n");
     for (int i = 0; i < NUM_PARTICLES_UNIVERSE; i++)
     {
@@ -175,42 +193,14 @@ int main()
         printf("Z: %f\n", particleList[i].vZ);
         printf("\n");
     }
-    FILE *fpAfter = fopen("After.dat", "w");
-    plot_particles(particleList, fpAfter);
-
-    printf("LJ MIN: %f\n", LJ_MIN);
-
-    end = omp_get_wtime();
-    cpu_time = (double)(end - start);
-
-    printf("Time Taken: %f seconds\n", cpu_time);
-    return 0;
 }
-void init_ParticleList(Particle *particleList){
-    srand(SEED);
-    int min = -10;
-    int max = 10;
+void plot_particles(Particle *particleList, FILE *fp)
+{
 
-
-    for(int i=0; i<NUM_PARTICLES_UNIVERSE;i++) {
-        particleList[i].x = ((float) rand() / (float) (RAND_MAX)) * UNIVERSE_SIZE;
-        particleList[i].y = ((float) rand() / (float) (RAND_MAX)) * UNIVERSE_SIZE;
-        particleList[i].z = ((float) rand() / (float) (RAND_MAX)) * UNIVERSE_SIZE;
-
-        particleList[i].vX = ((float)(min + rand() % (max - min + 1))/(float)max) * UNIVERSE_SIZE / 2;
-        particleList[i].vY = ((float)(min + rand() % (max - min + 1))/(float)max) * UNIVERSE_SIZE / 2;
-        particleList[i].vZ = ((float)(min + rand() % (max - min + 1))/(float)max) * UNIVERSE_SIZE / 2;
-
-        particleList[i].particleId = i;
+    for (int i = 0; i < NUM_PARTICLES_UNIVERSE; i++)
+    {
+        fprintf(fp, "%lf %lf %lf\n", particleList[i].x, particleList[i].y, particleList[i].z);
     }
+    fclose(fp);
 }
-/*
-void init_AccelerationCache(Acceleration *accelerationCache){
-    for(int i=0;i<NUM_PARTICLES_UNIVERSE;i++){
-        accelerationCache[i].particleId = i;
-        accelerationCache[i].aX = 0;
-        accelerationCache[i].aY = 0;
-        accelerationCache[i].aZ = 0;
-    }
-}
-*/
+
